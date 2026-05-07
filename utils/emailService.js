ccde9f19ca8f5708794eb804002814a9bc3eae09
muzammilshaikh4373 <muazmmilshaikh9881@gmail.com>
2026-05-07 12:29:36 +0530

    mail changes raley bravo

--- utils/emailService.js
+++ utils/emailService.js
@@ -13,7 +13,7 @@ const checkConfig = () => {
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
     auth: {
